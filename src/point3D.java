033c704b76cb94d9c0df953ca2c16d2751052501
sarmadashoor <sarmadashoor1@gmail.com>
2017-01-23 09:00:03 -0800

    Create point3D.java

---
+++ src/point3D.java
@@ -0,0 +1,21 @@
+package cs560project;
+
+public class point3D
+{
+  int x ; 
+  int y ; 
+  int z ;
+  
+  public point3D( ) 
+  {
+  }
+
+  public point3D( int ix , int iy , int iz ) 
+  {
+    x = ix ;
+    y = iy ; 
+    z = iz ;
+  }
+
+}
+
